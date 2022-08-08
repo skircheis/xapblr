1a81d446d226a275902fd494f99575d7de44b2be
Siegfried Kircheis <kircheis.siegfried@proton.me>
2022-08-08 19:40:46 +0200

    refactor: make flask dependencies optional

--- src/xapblr/server.py
+++ src/xapblr/server.py
@@ -1,5 +1,11 @@
-from xapblr_web import app
+try:
+    from xapblr_web import app
 
-def server(args):
-    app.run(host = args.host, port = args.port, debug=args.debug)
-    #raise NotImplementedError
+    def server(args):
+        app.run(host = args.host, port = args.port, debug=args.debug)
+
+except ModuleNotFoundError as e:
+    missing_module = e.name
+    def server(args):
+        from sys import exit
+        exit(f"xapblr: module {missing_module} is required for the web interface.")
