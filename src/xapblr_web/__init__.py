9f8034401f33333ce7b07281dc5bc46523ef5de1
Siegfried Kircheis <kircheis.siegfried@proton.me>
2023-03-02 00:19:30 +0100

    web: use cache dir in same location as dbs

--- src/xapblr_web/__init__.py
+++ src/xapblr_web/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_assets import Environment, Bundle
 from subprocess import Popen, PIPE
+from os import environ
+from pathlib import Path
+
+
+def get_data_dir():
+    try:
+        xdg_data_home = Path(environ["XDG_DATA_HOME"])
+    except KeyError:
+        xdg_data_home = Path(environ["HOME"]) / ".local/share"
+    return xdg_data_home / "xapblr"
 
 
 def pandoc_filter(_in, out, **kwargs):
@@ -11,7 +21,12 @@ def pandoc_filter(_in, out, **kwargs):
 
 app = Flask(__name__)
 
-assets = Environment(app)
+data_dir = get_data_dir()
+assets = Environment(app, )
+assets.directory=data_dir
+assets.load_path=[str(Path(__file__).parent / "static")]
+
+
 md = Bundle("SEARCH.md", filters=pandoc_filter, output="SEARCH.md.html")
 assets.register("md", md)
 css = Bundle("style.sass", filters="sass", output="style.css")
