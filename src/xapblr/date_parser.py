c777e642b49499ac8ac1e203432274f68e195557
nostalgebraist <nostalgebraist@gmail.com>
2023-08-03 09:20:03 -0700

    use dateparser + cast to int

--- src/xapblr/date_parser.py
+++ src/xapblr/date_parser.py
@@ -1,10 +1,13 @@
-import lark
-from datetime import datetime
-
+import dateparser
 
 def parse_date(date_str):
     # takes a human-readable datetime string as understood by notmuch and
     # returns a unix timestamp
     # Example inputs:
     # yesterday, 2 hours ago, last wednesday, 5 days ago, 1659706622
-    return datetime.strptime(date_str, "%Y-%m-%d").timestamp()
+    #
+    # note (nost) - dateparser works for most of the above but not "last wednesday"
+    # but it's quick and easy
+    dt = dateparser.parse(date_str)
+    if dt is not None:
+        return int(dt.timestamp())
