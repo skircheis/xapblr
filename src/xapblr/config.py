138bd1e9869cd22c48944defd32ce9c2d3856a6a
Siegfried Kircheis <kircheis.siegfried@proton.me>
2023-08-07 01:38:10 +0200

    fix: error messages in config.py
    were not printing to stderr correctly, and one was incorrect

--- src/xapblr/config.py
+++ src/xapblr/config.py
@@ -15,16 +15,17 @@ class Config:
             with key_file.open() as f:
                 self._config["api_key"] = load(f)
         except IOError as e:
-            print(stderr, f"Could not read API key from {key_file}: {e}")
+            print(f"Could not read API key from {key_file}: {e}", file=sys.stderr)
         except JSONDecodeError as e:
-            print(stderr, f"Malformed JSON in {key_file}: {e}")
+            # Don't need to exit, we could get a valid config from config.json
+            print(f"Malformed JSON in {key_file}: {e}", file=sys.stderr)
 
         try:
             with config_file.open() as f:
                 cfg = load(f)
                 self._config.update(cfg)
         except IOError as e:
-            print(stderr, f"Could not read API key from {key_file}: {e}")
+            print(f"Could not read config from {key_file}: {e}", file=sys.stderr)
         except JSONDecodeError as e:
             sys.exit(f"Malformed JSON in {config_file}: {e}")
 
