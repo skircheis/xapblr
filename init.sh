32d7cb0e05db63d77c88a509f5a4fbdce22bd475
Daniel Fitchmun <daniel@unimpossy.com>
2025-04-22 18:57:09 -0700

    add multi-blog support

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 source .venv/bin/activate
-xapblr index $BLOG_URL && systemctl --user enable --now xapblr-hourly@$BLOG_URL &
+
+if [ -n "${BLOG_URL+1}" ]; then 
+    xapblr index $BLOG_URL && systemctl --user enable --now xapblr-hourly@$BLOG_URL &
+fi
+
+touch /home/xapblr/blogs.txt
+while IFS= read -r blog_url; do
+    xapblr index $blog_url && systemctl --user enable --now xapblr-hourly@$blog_url 
+done < /home/xapblr/blogs.txt &
+
 uwsgi --ini ./config/uwsgi/xapblr.ini -H ./.venv
